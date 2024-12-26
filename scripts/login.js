3e17965861437b581623a61010cde2d7cea74c17
NagajyothiChukka <nagajyotichukka@gmail.com>
2024-12-26 15:41:54 +0530

    added login form

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,16 @@
 let form = document.getElementById("form");
 
-form.addEventListener("submit", function(){
-    
+form.addEventListener("submit", function () {
+    event.preventDefault();
+    let email = form.email.value;
+    let password = form.password.value;
+    // console.log(email);
+
+    if (email == "empher@gmail.com") {
+        if (password == "empher@123") {
+            alert("Login Success, you are redirecting to quiz page");
+            window.location.href = "quiz.html";
+        }
+    }
+
 })
